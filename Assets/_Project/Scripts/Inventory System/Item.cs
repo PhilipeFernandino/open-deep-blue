aaf5ccac52b10d50dcbb314c5384432025481049
PhilipeFernandino <philipegarciaf@gmail.com>
2024-03-22 21:18:05 -0300

    feat (inventory): stack ui

--- Assets/_Project/Scripts/Inventory System/Item.cs
+++ Assets/_Project/Scripts/Inventory System/Item.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0e25ca4a652eea355d6b99d9c45f12a902532f05129b277eaab22da38134edc
-size 1104
+oid sha256:32000708cc83bb8443b8045c130e0cd5abe3ec078e35add52a42c65828672b3a
+size 1356
