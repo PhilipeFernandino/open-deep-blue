d9887938cc0bb59dc85fefef3211714c982f70bc
PhilipeFernandino <philipegarciaf@gmail.com>
2024-04-22 23:47:39 -0300

    feat: player side anim

---
+++ Assets/_Project/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,57 @@
+﻿using Spine.Unity;
+using UnityEngine;
+
+namespace Core.Units
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        [SerializeField] private SkeletonAnimation _sideSkeletonAnimation;
+        [SerializeField] private SkeletonAnimation _frontSkeletonAnimation;
+
+        private Vector2 _input;
+
+        private bool _isSide = true;
+
+        private void Update()
+        {
+            GetInput();
+            if (_input.x != 0)
+            {
+                if (!_isSide)
+                {
+                    _isSide = true;
+                    _sideSkeletonAnimation.gameObject.SetActive(true);
+                    _frontSkeletonAnimation.gameObject.SetActive(false);
+                    _sideSkeletonAnimation.AnimationState.SetAnimation(0, "walk", true);
+                }
+
+                if (_input.x > 0)
+                {
+                    _sideSkeletonAnimation.skeleton.ScaleX = 1;
+                }
+                else
+                {
+                    _sideSkeletonAnimation.skeleton.ScaleX = -1;
+                }
+            }
+            else if (_input.y != 0)
+            {
+                if (_isSide)
+                {
+                    _isSide = false;
+                    _sideSkeletonAnimation.gameObject.SetActive(false);
+                    _frontSkeletonAnimation.gameObject.SetActive(true);
+                    _frontSkeletonAnimation.AnimationState.SetAnimation(0, "walk", true);
+                }
+            }
+
+        }
+
+        private void GetInput()
+        {
+            _input.x = Input.GetAxisRaw("Horizontal");
+            _input.y = Input.GetAxisRaw("Vertical");
+        }
+
+    }
+}
