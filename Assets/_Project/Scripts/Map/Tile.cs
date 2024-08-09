db9a9116c003ed8d32a7ccf05a8e62a8acc19322
PhilipeFernandino <philipegarciaf@gmail.com>
2024-08-08 23:27:45 -0300

    add: new tiles and trying out biomes

--- Assets/_Project/Scripts/Map/Tile.cs
+++ Assets/_Project/Scripts/Map/Tile.cs
@@ -3,15 +3,21 @@
     public enum Tile
     {
         None = 0,
+
+        // Stones and ores
         BlueStone = 1,
         CopperOre = 2,
         SilverOre = 3,
         MoonOre = 4,
         MoonStone = 5,
+        Amazonita = 6,
 
-        Chest,
-        AntQueenSpawn,
-        AntQueenRoomTile,
+        // Interactive 
+        Chest = 1001,
+
+        // Boss mark tiles
+        AntQueenSpawn = 2001,
+        AntQueenRoomTile = 2002,
 
     }
 }
